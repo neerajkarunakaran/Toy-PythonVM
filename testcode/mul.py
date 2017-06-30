67983248e57fa016d8fc927918bfabbe56c77c4f
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-06-30 22:07:24 +0530

    find multiple of numbers by recursion

--- testcode/mul.py
+++ testcode/mul.py
@@ -1,14 +1,8 @@
-
 def mult(n, mul):
     "print multiple of numbers byr recursion"
     if n == 1:
         return mul
     else:
         return mult(n-1, mul) + mul
-
-
-
-
    
 print mult(5, 45)
-
