c873757932f6e0e4e82ca18bdacfef0cda0ff4b8
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-06-30 22:05:44 +0530

    recursion

--- testcode/recursion.py
+++ testcode/recursion.py
@@ -1,9 +1,7 @@
-
 def recurr(loopnum,counter):
     if loopnum > 0:
         print "This is loop iteration",counter
         recurr(loopnum - 1,counter + 1)
     else:
         print "The loop is complete."
-
 recurr(10, 1)
