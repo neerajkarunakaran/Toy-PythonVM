563f0c20910cd2cfe70d5720c3e52f373d3ca8fe
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-06-30 13:59:51 +0530

    factors of number

---
+++ factor_number.py
@@ -0,0 +1,11 @@
+
+def factors_number(n):
+    print "factors of number are..."
+    x = n + 1
+    i = 1
+    while i < x:
+        if n % i == 0:
+            print i
+        i = i + 1
+
+factors_number(100)
