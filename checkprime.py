d1ec08f55c4bb24357feb104edc14d0f78aaa129
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-06-30 12:11:10 +0530

    check Number is prime or not

---
+++ checkprime.py
@@ -0,0 +1,15 @@
+
+def checkprime(n):
+    x = n // 2
+    k = 0
+    i = 2
+    while i < x:
+        i+=1
+        if n % i == 0:
+            k = k + 1
+    if k <= 0:
+        print "Number is prime"
+    else:
+        print "Number is not prime"
+
+checkprime(100)
