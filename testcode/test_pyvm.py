19dd5b1a4bd4b73fe48ed8b13aa20fb266faab18
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-07-01 03:39:34 +0530

    script for test pyvm: use '$ testpyvm.py <bytcodfile>'

--- testcode/test_pyvm.py
+++ testcode/test_pyvm.py
@@ -1,8 +1,9 @@
 import sys, os, subprocess
 
 def factorial_test(list):
+    """function will test the 
+    factorial.pyc output """ 
     text = list[0]
-
     expected = 479001600
     if int(text.strip()) == expected:
         print "factorial test: passed"
@@ -10,6 +11,8 @@ def factorial_test(list):
         print "factorial test: failed"
 
 def factor_test(list):
+    """function will test the
+    factor_number.pyc output"""
     result = []
     for text in list[1:]:
         result.append(int(text.strip()))
@@ -20,6 +23,8 @@ def factor_test(list):
         print "factor_number test: failed"
 
 def largest_test(list):
+    """function will test the 
+    large.pyc output """
     text = list[1]
     expected = 3234
     if int(text.strip()) == expected:
@@ -28,6 +33,8 @@ def largest_test(list):
         print "largest test: failed"
 
 def sumof_digit_test(list):
+    """function will test the
+    sumof_digits.pyc output"""
     text = list[0]
     expected = 25
     if int(text.strip()) == expected:
@@ -36,6 +43,8 @@ def sumof_digit_test(list):
         print "sumof_digits test: failed"
 
 def mul_test(list):
+    """function will test the
+    mul.pyc output"""
     text = list[0]
     expected = 225
     if int(text.strip()) == expected:
@@ -44,6 +53,8 @@ def mul_test(list):
         print "mul test: failed"
 
 def  recursion_test(list):
+    """function will test the 
+    recursion.pyc output"""
     text = list[0]
     expected = "This is loop iteration1"
     if text.strip() == expected:
@@ -52,6 +63,8 @@ def  recursion_test(list):
         print "recursion test: failed"
 
 def checkprime_test(list):
+    """function will test the 
+    checkprime.pyc output"""
     text = list[0]
     expected = "Number is not prime"
     if text.strip() == expected:
@@ -59,9 +72,13 @@ def checkprime_test(list):
     else:
         print "check prime test: failed"
 
+
 if __name__ == '__main__':
+
+
     filename = sys.argv[1]    
-f = subprocess.Popen(["/home/neeraj/neerajgit/pyvm/pyvm", filename], stdout = subprocess.PIPE)
+f = subprocess.Popen(["/home/neeraj/neerajgit/pyvm/pyvm", \
+    filename], stdout = subprocess.PIPE)
 list = f.stdout.readlines()
 file = filename.split(".")
 if file[0] == "checkprime":
