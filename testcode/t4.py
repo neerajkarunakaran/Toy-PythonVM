c9d59aa2302c07241fda3f9967d05cbf02652717
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-06-30 22:10:35 +0530

    testcode

--- testcode/t4.py
+++ testcode/t4.py
@@ -1,21 +1,7 @@
-
-
-
-
-
-
-
-
-
-
 d = 2
 e = 2
 a =9
 b = -457
-
-
-
-
 a = d == e
 print a
 a = b > d
@@ -34,40 +20,15 @@ a = 9 - b
 print a
 d = b*a
 print d
-
-
 a= 34567
 b =- 344
 c= -7896
 print a+b+c
-
 print a / b   
-
-
 print d-c-e
-
-
 a= 456
 b = 56778 
 c = 345
 d = -4567 
 e = 4567
 print a+b+c+d+e
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
