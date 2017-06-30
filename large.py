cddc333e23639920a0958d29597b3fe680801083
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-06-30 13:20:22 +0530

    largest among three numbers

---
+++ large.py
@@ -0,0 +1,12 @@
+
+def large(n1, n2, n3):
+    if n1 >= n2 and n1 >= n3:
+        large = n1
+    elif n2 >= n1 and n2 >= n3:
+        large = n2
+    else:
+        large = n3
+    print "largest is"
+    print large
+
+large(1234,2000,3234)
