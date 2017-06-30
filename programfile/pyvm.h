b2fcc4a6e3ba9f6db43ebf929c7b8d9ed3d8f56a
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-06-30 22:01:03 +0530

    refactored

--- programfile/pyvm.h
+++ programfile/pyvm.h
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/stat.h>
 #include <string.h>
-char r_byte(FILE * p);		/* read a byte */
+char r_byte(FILE * p);		/*  read a byte */
 long r_value(int n, FILE * p);	/* read bytes and convert to long */
 static char *r_bytes(int n, FILE * p);	/* read bytes as string */
 int add(int x, int y);		/* addition function */
